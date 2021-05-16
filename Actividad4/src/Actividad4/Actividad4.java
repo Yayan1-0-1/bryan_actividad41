81ea0e8767ba6cc84f559bc9a33201eb9a5de7ec
Bryan <Bryan@192.168.1.187>
2021-05-16 19:25:05 +0200

    commit3

--- Actividad4/src/Actividad4/Actividad4.java
+++ Actividad4/src/Actividad4/Actividad4.java
@@ -4,5 +4,6 @@ public class Actividad4 {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo. Java Git.");
 		System.out.println("Hola mundo. Java Git.");
+		System.out.println("Hola mundo. Java Git.");
 		}
 }
