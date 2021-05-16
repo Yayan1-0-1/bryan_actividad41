c33273c3cbe07d8e5ab9b7a46856bcb0198d1fbd
Bryan <Bryan@192.168.1.187>
2021-05-16 19:24:48 +0200

    Commit2

--- Actividad4/src/Actividad4/Actividad4.java
+++ Actividad4/src/Actividad4/Actividad4.java
@@ -3,5 +3,6 @@ package Actividad4;
 public class Actividad4 {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo. Java Git.");
+		System.out.println("Hola mundo. Java Git.");
 		}
 }
