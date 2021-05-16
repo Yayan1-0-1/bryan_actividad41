6ebbb9cfb90e24aca1c04b7ed9cbd6491e1b89d8
Bryan <Bryan@192.168.1.187>
2021-05-16 19:46:08 +0200

    commit 4

--- Actividad4/src/Actividad4/otraclanueva.java
+++ Actividad4/src/Actividad4/otraclanueva.java
@@ -5,6 +5,7 @@ public class otraclanueva {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo. Java Git.");
 		System.out.println("Hola mundo. Java Git.");
+		System.out.println("Hola mundo. Java Git.");
 	}
 
 }
